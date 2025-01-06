f73f0c14fe51067c5acabcd5e1848be34a62dfd3
Alexander Yastrebov <yastrebov.alex@gmail.com>
2025-01-06 17:42:08 +0100

    Add BenchmarkFindPublicKey

--- main_test.go
+++ main_test.go
@@ -3,7 +3,10 @@ package main
 import (
 	"crypto/ecdh"
 	"crypto/rand"
+	"io"
 	"testing"
+
+	"filippo.io/edwards25519"
 )
 
 func BenchmarkGenerateKey(b *testing.B) {
@@ -11,4 +14,23 @@ func BenchmarkGenerateKey(b *testing.B) {
 		priv, _ := ecdh.X25519().GenerateKey(rand.Reader)
 		_ = priv.PublicKey().Bytes()
 	}
+}
+
+func BenchmarkFindPublicKey(b *testing.B) {
+	key := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		b.Fatal(err)
+	}
+
+	s0, err := edwards25519.NewScalar().SetBytesWithClamping(key)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	i := 0
+	findPublicKey(s0, func(p []byte) bool {
+		_ = p[0] + p[1] + p[2]
+		i++
+		return i == b.N
+	})
 }
